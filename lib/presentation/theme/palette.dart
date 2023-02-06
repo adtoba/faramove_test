c4a91fab915da45b229dfd6232abfbc3e5d4eb37
adtoba <adetoba53@gmail.com>
2023-02-06 13:12:17 +0100

    chore: Declare color palette

---
+++ lib/presentation/theme/palette.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color white = Colors.white;
+  static const Color lightBlue = Color(0xffF1F6FE);
+  static const Color blue = Color(0xff3079EC);
+  static const Color black = Color(0xff3079EC);
+
+  static const Color lightGrey = Color(0xff888D95);
+  static const Color purple = Color(0xff7444EE);
+  static const Color brown = Color(0xffEE7444);
+  static const Color orange = Color(0xffEEAD44);
+}
